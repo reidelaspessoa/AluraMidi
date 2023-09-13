ab7d26054d9f0b0fd32087282d8d897ed99f8fe0
reidelaspessoa <107411087+reidelaspessoa@users.noreply.github.com>
2023-09-13 09:52:57 -0300

    Add files via upload

--- AluraMidi-main/aluramidi-curso-arquivos-iniciais/main.js
+++ AluraMidi-main/aluramidi-curso-arquivos-iniciais/main.js
@@ -1,4 +1,14 @@
 function tocaSomPom(){
     document.querySelector('#som_tecla_pom').play();
 }
-document.querySelector('.tecla_pom');
+
+const listaDeTeclas = document.querySelectorAll('.tecla');
+
+listaDeTeclas[0].onclick = tocaSomPom;
+
+let contador = 0;
+
+// Estrutura de repetição - Enquanto
+while () {
+    
+}
